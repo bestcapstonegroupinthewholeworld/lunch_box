77f0ce23a35bb8e2b379ae44fdbc617c0982252e
Elena Pushkar <elena@epushkar.com>
2021-09-23 18:01:09 -0500

    party lobby tweaks

--- client/components/PartyLobby.js
+++ client/components/PartyLobby.js
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import SVGFile from './PartyLobbySvg'
+import VideoCall from '../components/VideoCall';
+
+import { fetchCards, addCard } from '../store/lunchbox';
+import { makeRandomTeams } from '../store/party';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import VideoCall from '../components/VideoCall';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { connect } from 'react-redux';
-import { fetchCards, addCard } from '../store/lunchbox';
-import { makeRandomTeams } from '../store/party';
 
 /** STYLES **/
 const useStyles = makeStyles((theme) => ({
   partyLobbyOuter: {
     position: 'relative',
-    height: 'calc(100vh - 100px)',
+    height: 'calc(100vh - 130px)',
   },
   partyLobbyVideos: {
-    height: 'calc((100vh - 100px)/2)',
+    height: 'calc((100vh - 130px)/2)',
     display: 'block',
     flexWrap: 'wrap',
     position: 'relative',
+    marginLeft: '3em'
   },
-  partyLobbyBottom: {},
+  partyLobbyBottom: {
+    marginLeft: '3em'
+  },
 }));
 
 /** COMPONENT **/
@@ -63,43 +69,58 @@ const PartyLobby = ({
 
   return (
     <Box className={classes.partyLobbyOuter} ml={6} mr={6} pl={4} pr={4}>
+      <div className="svgFile">
+        <SVGFile />
+      </div>
+
       <Box textAlign="center">
         <h1> Party Lobby </h1>
       </Box>
       <Box className={classes.partyLobbyVideos}>
-        <VideoCall />
+        <div className="partyLobbyVideoSection">
+          <VideoCall />
+        </div>
       </Box>
       <Box className={classes.partyLobbyBottom}>
         <Grid container spacing={2}>
           <Grid container item xs={6} md={6}>
-            <TextField
-              hiddenLabel
-              id="filled-hidden-label-normal"
-              variant="filled"
-              label="Enter a Word"
-              value={word}
-              onChange={(event) => setWord(event.target.value)}
-            />
-            <Button
-              color="secondary"
-              variant="contained"
-              size="large"
-              disabled={!word || game.cardsPerPlayer - userCards.length === 0}
-              onClick={addToBox}
-            >
-              Add a Word
-            </Button>
-            <h3>{game.cardsPerPlayer - userCards.length} Left!</h3>
+          { 
+            userCards.length !== 10 
+              ? 
+                <div className="enterTheWord">
+                  <TextField
+                    hiddenLabel
+                    id="filled-hidden-label-normal"
+                    variant="filled"
+                    label="Enter a Word"
+                    value={word}
+                    onChange={(event) => setWord(event.target.value)}
+                  />
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    size="large"
+                    disabled={!word || game.cardsPerPlayer - userCards.length === 0}
+                    onClick={addToBox}
+                  >
+                  Add a Word
+                  </Button>
+                  <h4 className="italicBlue"> Add items to your LunchBox. <span className="accentYellow">{(game.cardsPerPlayer ? game.cardsPerPlayer : 10) - (userCards.length ? userCards.length : 0)}</span> words left!</h4>
+                </div>
+              :
+                <h3> All words have been added! </h3>
+          }
           </Grid>
-          <Grid container item xs={6} md={6}></Grid>
+         <Box>
           <Button
-            color="secondary"
-            variant="contained"
-            size="large"
-            onClick={createTeams}
-          >
-            Create Teams
+             color="secondary"
+             variant="contained"
+             size="large"
+             onClick={createTeams}
+           >
+             Create Teams
           </Button>
+         </Box>  
         </Grid>
       </Box>
     </Box>
--- client/components/video.js
+++ client/components/video.js
@@ -2,10 +2,18 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 import { AgoraVideoPlayer } from "agora-rtc-react";
+import {
+  config,
+  useClient,
+  useMicrophoneAndCameraTracks,
+  channelName,
+} from "../settings.js";
 
 import { Grid } from "@material-ui/core";
 import Box from '@material-ui/core/Box'
 import { makeStyles } from '@material-ui/core/styles'
+
+import Control from "./control";
 
 const useStyles = makeStyles((theme) => ({
   videoContainerOuter: {
@@ -38,6 +46,10 @@ const useStyles = makeStyles((theme) => ({
 export default function Video(props) {
   const { users, tracks } = props;
   const [gridSpacing, setGridSpacing] = useState(12);
+  const [start, setStart] = useState(false);
+  const [inCall, setInCall] = useState(false);
+  const { ready } = useMicrophoneAndCameraTracks();
+
   const classes = useStyles();
 
   useEffect(() => {
@@ -51,15 +63,24 @@ export default function Video(props) {
       <Grid container className={classes.videoContainerOuter}>
       <Grid item xs={gridSpacing} className={classes.videoHost}>
         {/* host ===================== */}
-        <AgoraVideoPlayer
-          videoTrack={tracks[1]}
-          style={{
-            height: "100%",
-            width: "100%",
-            position: "absolute",
-          }}
-          className={classes.singleVideoScreenHost}
-        />
+        <div className="team-one">
+          <AgoraVideoPlayer
+            videoTrack={tracks[1]}
+            style={{
+              height: "100%",
+              width: "100%",
+              position: "absolute",
+              borderRadius: '15px',
+            }}
+          />
+        </div>
+        {/* {ready && tracks && (
+                  <Control
+                    tracks={tracks}
+                    setStart={setStart}
+                    setInCall={setInCall}
+                  />
+                )} */}
       </Grid>
       {/* generate a video for every sigle user  */}
       {users.length > 0 &&
@@ -67,16 +88,28 @@ export default function Video(props) {
           if (user.videoTrack) {
             return (
               <Grid item xs={gridSpacing} className={classes.singleVideoScreen}>
-                <AgoraVideoPlayer
+                 <div className="team-one">
+                 <AgoraVideoPlayer
                   key={user.uid}
                   videoTrack={user.videoTrack}
                   style={{
                     height: "100%",
                     width: "100%",
-                    position: "absolute"
+                    position: "absolute",
+
                   }}
                   className={classes.singleVideoScreenInner}
                 />
+
+                 </div>
+               
+                {/* {ready && tracks && (
+                  <Control
+                    tracks={tracks}
+                    setStart={setStart}
+                    setInCall={setInCall}
+                  />
+                )} */}
               </Grid>
             );
           } else {

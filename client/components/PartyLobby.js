25a5d1c62409194a449434fbffd7aeba6251471b
Brian Smolin <briansmolin@yahoo.com>
2021-09-24 16:27:17 -0400

    tiny fix

--- client/components/PartyLobby.js
+++ client/components/PartyLobby.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
 
-import SVGFile from './PartyLobbySvg'
+import SVGFile from './PartyLobbySvg';
 import VideoCall from '../components/VideoCall';
 
 import { fetchCards, addCard } from '../store/lunchbox';
-import { makeRandomTeams } from '../store/party';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
@@ -14,10 +12,9 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
 import { connect } from 'react-redux';
-import { fetchCards, addCard } from '../store/lunchbox';
+
 import { makeRandomTeams, getPartyInfo } from '../store/party';
 import { useParams } from 'react-router-dom';
-
 
 /** STYLES **/
 const useStyles = makeStyles((theme) => ({
@@ -30,10 +27,10 @@ const useStyles = makeStyles((theme) => ({
     display: 'block',
     flexWrap: 'wrap',
     position: 'relative',
-    marginLeft: '3em'
+    marginLeft: '3em',
   },
   partyLobbyBottom: {
-    marginLeft: '3em'
+    marginLeft: '3em',
   },
 }));
 
@@ -89,43 +86,51 @@ const PartyLobby = ({
       <Box className={classes.partyLobbyBottom}>
         <Grid container spacing={2}>
           <Grid container item xs={6} md={6}>
-          { 
-            userCards.length !== 10 
-              ? 
-                <div className="enterTheWord">
-                  <TextField
-                    hiddenLabel
-                    id="filled-hidden-label-normal"
-                    variant="filled"
-                    label="Enter a Word"
-                    value={word}
-                    onChange={(event) => setWord(event.target.value)}
-                  />
-                  <Button
-                    color="primary"
-                    variant="contained"
-                    size="large"
-                    disabled={!word || game.cardsPerPlayer - userCards.length === 0}
-                    onClick={addToBox}
-                  >
+            {userCards.length !== 10 ? (
+              <div className="enterTheWord">
+                <TextField
+                  hiddenLabel
+                  id="filled-hidden-label-normal"
+                  variant="filled"
+                  label="Enter a Word"
+                  value={word}
+                  onChange={(event) => setWord(event.target.value)}
+                />
+                <Button
+                  color="primary"
+                  variant="contained"
+                  size="large"
+                  disabled={
+                    !word || game.cardsPerPlayer - userCards.length === 0
+                  }
+                  onClick={addToBox}
+                >
                   Add a Word
-                  </Button>
-                  <h4 className="italicBlue"> Add items to your LunchBox. <span className="accentYellow">{(game.cardsPerPlayer ? game.cardsPerPlayer : 10) - (userCards.length ? userCards.length : 0)}</span> words left!</h4>
-                </div>
-              :
-                <h3> All words have been added! </h3>
-          }
+                </Button>
+                <h4 className="italicBlue">
+                  {' '}
+                  Add items to your LunchBox.{' '}
+                  <span className="accentYellow">
+                    {(game.cardsPerPlayer ? game.cardsPerPlayer : 10) -
+                      (userCards.length ? userCards.length : 0)}
+                  </span>{' '}
+                  words left!
+                </h4>
+              </div>
+            ) : (
+              <h3> All words have been added! </h3>
+            )}
           </Grid>
-         <Box>
-          <Button
-             color="secondary"
-             variant="contained"
-             size="large"
-             onClick={createTeams}
-           >
-             Create Teams
-          </Button>
-         </Box>  
+          <Box>
+            <Button
+              color="secondary"
+              variant="contained"
+              size="large"
+              onClick={createTeams}
+            >
+              Create Teams
+            </Button>
+          </Box>
         </Grid>
       </Box>
     </Box>

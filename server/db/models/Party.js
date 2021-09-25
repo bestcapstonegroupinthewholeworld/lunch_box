d61bff6c1e864005a58ea794ec02d530bbf7bdf0
Brian Smolin <briansmolin@yahoo.com>
2021-09-25 12:56:30 -0400

    into round with cluegiver

--- server/db/models/Party.js
+++ server/db/models/Party.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const { UUID, UUIDV4, INTEGER } = Sequelize;
+const { UUID, UUIDV4, INTEGER, STRING } = Sequelize;
 const db = require('../db');
 
 const Party = db.define('party', {
@@ -9,9 +9,10 @@ const Party = db.define('party', {
     defaultValue: UUIDV4(),
   },
   password: {
-    type: Sequelize.STRING,
+    type: STRING,
   },
   guessingTeam: INTEGER,
+  currentRoute: STRING,
 });
 
 module.exports = Party;
@@ -47,34 +48,4 @@ Party.prototype.chooseGame = async function (gameName) {
   const lunchbox = await db.models.lunchbox.create({ gameId: game.id });
 
   return { lunchbox, game };
-};
-
-Party.prototype.makeRandomTeams = async function () {
-  const users = await db.models.user.findAll({ where: { partyId: this.id } });
-  const team1 = await db.models.team.create({ partyId: this.id });
-  const team2 = await db.models.team.create({ partyId: this.id });
-
-  this.guessingTeam = team1.id;
-  this.save();
-
-  const teamSize = users / 2;
-
-  const team1users = [];
-
-  for (let i = 0; i < teamSize; i++) {
-    const idx = Math.round(Math.random() * users.length);
-    team1users.push(users[idx]);
-    users.splice(idx, 1);
-  }
-
-  users.forEach(async (user, idx) => {
-    user.teamId = team2.id;
-    user.turnOrder = idx + 1;
-    await user.save();
-  });
-  team1users.forEach(async (user, idx) => {
-    users.teamId = team1.id;
-    user.turnOrder = idx + 1;
-    await user.save();
-  });
 };

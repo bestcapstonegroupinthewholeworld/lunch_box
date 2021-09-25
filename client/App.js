2be97814e7f3168c1e90203b55d95ab9dd18d546
Elena Pushkar <elena@epushkar.com>
2021-09-25 12:05:09 -0500

    floating circles, design tweaks component

--- client/App.js
+++ client/App.js
@@ -34,9 +34,13 @@ const theme = createTheme({
     MuiButton: {
       containedPrimary: {
         color: "white",
+        width: '170px',
+        borderRadius: '20px'
       },
       containedSecondary: {
         color: "white",
+        width: '170px',
+        borderRadius: '20px'
       },
     },
   },
@@ -49,19 +53,19 @@ const theme = createTheme({
 
 const useStyles = makeStyles((theme) => ({
   app: {
-    // minHeight: "100vh",
-    // background: "rgb(243,185,63)",
-    // background:
-    //   "linear-gradient(90deg, rgba(243,185,63,1) 0%, rgba(96,210,218,1) 100%)",
-    // "&::before": {
-    //   content: '""',
-    //   backgroundColor: "#141018",
-    //   position: "absolute",
-    //   height: "calc(100% - 60px)",
-    //   width: "calc(100% - 60px)",
-    //   top: "30px",
-    //   left: "30px",
-    // },
+    minHeight: "100vh",
+    background: "rgb(243,185,63)",
+    background:
+      "linear-gradient(90deg, rgba(243,185,63,1) 0%, rgba(96,210,218,1) 100%)",
+    "&::before": {
+      content: '""',
+      backgroundColor: "#141018",
+      position: "absolute",
+      height: "calc(100% - 60px)",
+      width: "calc(100% - 60px)",
+      top: "30px",
+      left: "30px",
+    },
   },
 }));
 
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import { Link } from 'react-router-dom'
+
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import { withStyles } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography"
-import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
+
+import Circles from './FloatingCircles'
+
 
 /** STYLES **/
 const WhiteTextTypography = withStyles({
@@ -44,40 +48,38 @@ const WhiteTextTypography = withStyles({
 export const Home = props => {
   const {username} = props;
   const classes = useStyles();
-
+  
   return (
     <div className={classes.homeOuter}>
-      <Grid>
-        <Box ml={6} pl={6} className={classes.homeMain} alignItems="center">
-          <Grid container spacing={2} className={classes.homeInner}>
-            <Grid item xs={6} md={7} p={4}>
-              <Box mb={2}>
-                <p> Hi there, {username} </p>
-                <WhiteTextTypography variant="h2">
-                    Let's play despite <br></br> the distance!
+      <Circles wrappedComponentRef={ref => this.myRef = ref} />
+        <Grid>
+          <Box ml={6} pl={6} className={classes.homeMain} alignItems="center">
+            <Grid container spacing={2} className={classes.homeInner}>
+              <Grid item xs={6} md={7} p={4}>
+                <Box mb={2}>
+                  <h3>Hi there, <span className="accentYellow">{username.toUpperCase()}</span> </h3> 
+                  <WhiteTextTypography variant="h2">
+                      Let's play despite <br></br> the distance!
+                  </WhiteTextTypography>
+                </Box>      
+                <WhiteTextTypography pt={2} pb={3} variant="h5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 </WhiteTextTypography>
-              </Box>      
-              <WhiteTextTypography pt={2} pb={3} variant="h5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              </WhiteTextTypography>
-              <Box mt={3}>
-                <Grid container spacing={2} mt={3} className={classes.buttons}>
-                  <Button color='primary' variant='contained' size='large' component={ Link } to="/party">
-                    Get Started! 
-                  </Button>
-                </Grid>  
-              </Box>
+                <Box mt={3}>
+                  <Grid container spacing={2} mt={3} className={classes.buttons}>
+                    <Button color='primary' variant='contained' size='large' component={ Link } to="/party">
+                      Get Started! 
+                    </Button>
+                  </Grid>  
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-      </Grid>
+          </Box>
+        </Grid>
     </div>
   )
 }
 
-/**
- * CONTAINER
- */
  const mapState = state => {
   return {
     username: state.auth.username
